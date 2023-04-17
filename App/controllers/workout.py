8cb1e05e20569e7c71b720ff419ede4b3c9a0904
D-e-v-M <97921518+D-e-v-M@users.noreply.github.com>
2023-04-17 11:01:15 -0400

    Add files via upload

---
+++ App/controllers/workout.py
@@ -0,0 +1,24 @@
+from App.models import User,Workout,UserWorkout
+from App.database import db
+
+def get_workouts():
+    workouts=Workout.query.all()
+
+    return workouts
+
+def get_workout(workout_id=1):
+    workout=Workout.query.get(workout_id)
+
+    return workout
+
+def add_workout_to_user(user_id,workout_id):
+    workout=Workout.query.filter_by(id=workout_id).first()
+    user=User.query.filter_by(id=user_id).first()
+
+    user.add_workout(workout.id)
+
+def remove_workout_from_user(user_id,user_workout_id):
+    workout=UserWorkout.query.filter_by(id=user_workout_id).first()
+    user=User.query.filter_by(id=user_id).first()
+
+    user.remove_workout(workout.id) 
